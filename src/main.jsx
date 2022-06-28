abda52595e60845fb01e2020a8bf1a2f262308f1
Victor Cortes <victor.cortes@softtek.com>
2022-06-28 15:05:05 -0500

    begin section 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CounterApp } from "./CounterApp";
 import { FirstApp } from './FirstApp';
 import { App } from "./HelloWorldApp";
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <FirstApp title='Hola, soy Goku'  />
+        {/* <FirstApp title='Hola, soy Goku'  /> */}
+        <CounterApp value={0}/>
     </React.StrictMode>
 );
